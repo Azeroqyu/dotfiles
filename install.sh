d44cbe5a42cbc6a7333194a64496017dee6b98a5
Azeroqyu <zerosucksatlife@gmail.com>
2025-05-10 23:22:04 -0300

    kitty config, nvim fixes

--- install.sh
+++ install.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 #This script installs the dependencies needed for my dotfiles
-declare -a PKGS=(nvim alacritty hypr mako starship waybar)
+declare -a PKGS=(nvim alacritty hypr mako starship waybar tmux kitty)
 #checking if the packages needed are installed, i made this for arch, change "yay" for your prefered package manager.
 echo "installing dependencies..."
-
-yay -S --noconfirm --needed - < dependencies.list
-	
+sudo pacman -S --noconfirm --needed - < dependencies.list
 #applying the theme
 read -p "would you like to apply my theming? [Y/n]" yn
 case $yn in
-	[yY] )  echo "Proceeding with instalation...";
+	[yY] )	echo "Proceeding with instalation...";
+		stow .
+		stow $PKGS
+		;;
+	[nN] )	echo "Instalation cancelled, exiting...";
+		;;
+	*)	echo "Proceeding with instalation...";
 		stow $PKGS
 		stow .
-		;;
-	[nN] )echo "Instalation cancelled, exiting...";
 		;;
 esac
 
